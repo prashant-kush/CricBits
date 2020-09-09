d48f11c1348fea02d366512e7a8bb8b161430689
Prashant <prashantk.tps@gmail.com>
2020-09-10 01:14:07 +0530

    Updates

---
+++ precache-manifest.f34368cf4ad882592c1cf92161cd9d90.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579e24dc272f7493685ececc0d075ec5",
+    "url": "/cric-bits/index.html"
+  },
+  {
+    "revision": "97eba12e9b2366acabb3",
+    "url": "/cric-bits/static/css/4.690970d0.chunk.css"
+  },
+  {
+    "revision": "c042079856a8b5c1b078",
+    "url": "/cric-bits/static/css/5.690970d0.chunk.css"
+  },
+  {
+    "revision": "8294eed6489477bc718d",
+    "url": "/cric-bits/static/css/6.4ffbe6bf.chunk.css"
+  },
+  {
+    "revision": "ad6b0af83cdd599c6563",
+    "url": "/cric-bits/static/css/7.1610f3b2.chunk.css"
+  },
+  {
+    "revision": "81794775d907fd0d9f04",
+    "url": "/cric-bits/static/css/main.16bdd15c.chunk.css"
+  },
+  {
+    "revision": "efa0103e95671e076a4e",
+    "url": "/cric-bits/static/js/0.8b673d91.chunk.js"
+  },
+  {
+    "revision": "8c4a52c548d60367d74557f7ce242617",
+    "url": "/cric-bits/static/js/0.8b673d91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb93ecdfd14fd901afa",
+    "url": "/cric-bits/static/js/3.df621b92.chunk.js"
+  },
+  {
+    "revision": "cbf7ab8ad9b210a8e3f9021a24e25fd5",
+    "url": "/cric-bits/static/js/3.df621b92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97eba12e9b2366acabb3",
+    "url": "/cric-bits/static/js/4.0f6a8701.chunk.js"
+  },
+  {
+    "revision": "c042079856a8b5c1b078",
+    "url": "/cric-bits/static/js/5.77ba67be.chunk.js"
+  },
+  {
+    "revision": "8294eed6489477bc718d",
+    "url": "/cric-bits/static/js/6.4958b77e.chunk.js"
+  },
+  {
+    "revision": "ad6b0af83cdd599c6563",
+    "url": "/cric-bits/static/js/7.3075134c.chunk.js"
+  },
+  {
+    "revision": "81794775d907fd0d9f04",
+    "url": "/cric-bits/static/js/main.d8d8aea6.chunk.js"
+  },
+  {
+    "revision": "b5cfe6ed02f5be7a34f1",
+    "url": "/cric-bits/static/js/runtime-main.f369728c.js"
+  },
+  {
+    "revision": "739b6ae9353d0c97ba538b77632f81b3",
+    "url": "/cric-bits/static/media/Group 3.739b6ae9.svg"
+  }
+]);
